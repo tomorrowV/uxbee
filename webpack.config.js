ec7a623ec4070925c1fbdbe53f147c616337da8e
Kolya Cherevan <cherevan.n.s@gmail.com>
2020-09-25 17:27:45 +0300

    addin wp

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+       test:/\.(s*)css$/,
+       use: [
+          miniCss.loader,
+          'css-loader',
+          'sass-loader',
+       ]
+    }]
+ },
+ plugins: [
+    new miniCss({
+       filename: 'style.css',
+    }),
+ ]
+};
