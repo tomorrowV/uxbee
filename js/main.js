3353216bbc6d59b1ce9ff54e2eee7cd697f0b472
Vlad <60934839+tomorrowV@users.noreply.github.com>
2020-09-25 15:50:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $('.header__burger').click(function(event) {
+      $('.header__burger, .header__menu, .top, .bottom, .home, .work,.process').toggleClass('active');
+      $('body').toggleClass('lock');
+  })
+})
+
+
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+};
+
+
+function toggle() {
+    let trailer = document.querySelector(".trailer");
+    let video = document.querySelector("video");
+    let home = document.querySelector('body');
+    home.classList.toggle('bg')
+    trailer.classList.toggle("active");
+    video.pause();
+    video.currentTime = 0;
+};
+
